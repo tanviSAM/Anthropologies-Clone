5127102185aebd1da93d3920e0e1a5d82e9f8313
Tanvi Samant <96070177+tanviSAM@users.noreply.github.com>
2022-01-21 00:31:42 +0530

    Add files via upload

---
+++ Shoes.js
@@ -0,0 +1,274 @@
+var shoeData= [
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/64585938_026_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Shoe The Bear Rebel Lug Boots",
+        price:"$136",
+        p:136,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/67176966_011_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Nisolo Ama Woven Mules",
+        price:"$180",
+        p:180,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/67172148_045_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Nisolo All-Day Clogs",
+        price:"$160",
+        p:160,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/65219214_001_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Silent D Hunter Western Clogs",
+        price:"$140",
+        p:140,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/64191091_224_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Marc Fisher Square-Toe Heeled Mules",
+        price:"$105",
+        p:105,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/66266362_060_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Jeffrey Campbell Woven Mules",
+        price:"$200",
+        p:200,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/65220758_066_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Marc Fisher Puffy Sandals",
+        price:"$150",
+        p:150,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/65586471_014_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Mystique Embellished Slide Sandals",
+        price:"$120",
+        p:120,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/67753715_052_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Swedish Hasbeens Viola Clog Boots",
+        price:"$450",
+        p:450,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/65386773_014_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Silent D Jayman Lace-Up Heels",
+        price:"$200",
+        p:200,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/66045576_015_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Plush Faux Fur Slippers",
+        price:"$28",
+        p:28,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/66057159_007_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Charles David Reveal Mules",
+        price:"$99",
+        p:99,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/66057365_068_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Charles David Embellished Ankle-Strap Heels",
+        price:"$99",
+        p:99,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/64209752_072_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Schutz Camurca Heeled Sandals",
+        price:"$108",
+        p:108,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/66398892_001_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Matisse Floral Platform Mules",
+        price:"$50",
+        p:50,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/63372387_001_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Matisse Caty Western Boots",
+        price:"$200",
+        p:200,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/57064404_002_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Veja Campo Leather Sneakers",
+        price:"$145",
+        p:145,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/65222085_001_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Silent D Gideota Suede Mules",
+        price:"$140",
+        p:140,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/61399259_025_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"On Cloud Monochrome Running Sneakers",
+        price:"$130",
+        p:130,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/65127771_072_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Vicenza Puffy Heeled Sandals",
+        price:"$160",
+        p:160,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/65128530_012_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Vicenza Braided Platform Heels",
+        price:"$175",
+        p:175,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/65572687_055_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Alohas South Heeled Boots",
+        price:"$225",
+        p:225,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/65825747_071_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Seychelles Breath Of Fresh Air Puffy Sandals",
+        price:"$69",
+        p:69,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/62159975_065_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Veja Rio Branco Sneakers",
+        price:"$140",
+        p:140,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/63652846_025_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Seychelles Hometown Heeled Ankle Boots",
+        price:"$159",
+        p:159,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/59485425_036_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Veja Condor Sneakers",
+        price:"$160",
+        p:160,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/66265901_034_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Jeffrey Campbell Landen Sandals",
+        price:"$155",
+        p:155,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/63982177_038_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Bombas Jacquard Grip Slippers",
+        price:"$40",
+        p:40,
+    },
+]
+
+var cart = JSON.parse(localStorage.getItem("basketItem")) || [];
+
+clothingData(shoeData);
+
+//Sorting by Price
+
+function PriceSort(){
+    var select= document.querySelector("#priceSort").value;
+    console.log(select);
+
+    if (select == "high"){
+        shoeData.sort(function (a, b) {
+            return Number(b.p) - Number(a.p);
+
+        });
+    }
+    if(select == "low"){
+        shoeData.sort(function (a, b) {
+            return Number(a.p) - Number(b.p);
+
+        });
+    }
+    // console.log(product);
+    clothingData(shoeData);
+}
+
+//Clothing Page Data
+
+function clothingData(shoeData){
+    document.querySelector("#ProductDisplay").textContent ="";
+    shoeData.map(function (elem){
+    
+        var div1  = document.createElement('div');
+        
+        //Product Display
+        var imagediv  = document.createElement('div');
+        var img = document.createElement('img');
+        img.setAttribute('src',elem.image_url);
+        img.setAttribute('id','Product')
+        imagediv.append(img);
+    
+        //Product Details
+        var namediv  = document.createElement('div');
+        var namehead = document.createElement('p');
+        namehead.textContent = elem.name;
+        namediv.append(namehead);
+        
+        var rate  = document.createElement('p');
+        var mrpdiv  = document.createElement('div');
+        rate.textContent = elem.price;
+        mrpdiv.append(rate);
+        
+        var button = document.createElement('button');
+        button.setAttribute('id','btn');
+        button.textContent = "Quick Pick";
+        button.addEventListener("click", function(){
+            addCart(elem);
+        });
+        
+        
+        div1.append(imagediv,namediv,mrpdiv,button); 
+        document.querySelector('#ProductDisplay').append(div1);
+        
+    })
+}
+
+//Cart
+var cart=[];
+
+function addCart(elem){
+    console.log(elem);
+    cart.push(elem);
+    localStorage.setItem("basketItem", JSON.stringify(cart))
+    alert("Added Item to Basket");
+}
