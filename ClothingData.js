0a8e04d88874f2a403bad20efe5f04ecf7f059ea
Tanvi Samant <96070177+tanviSAM@users.noreply.github.com>
2022-01-20 22:41:44 +0530

    Add files via upload

---
+++ ClothingData.js
@@ -0,0 +1,483 @@
+ //Clothing Page
+ var prodData=[
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4133445000030_224_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Cozy Sherpa Jacket",
+        color:"Creme",
+        type:"Jacket/Hoodies",
+        size:"XL",
+        price:"$229",
+        p:229,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4130370060052_095_b3?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Sequined Embroidered Mini Dress",
+        color:"Abstract",
+        type:"Dress",
+        size:"S",
+        price:"$248",
+        p:248,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/65883670_050_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"L Space Take A Hike Pullover",
+        color:"Purple",
+        type:"Tops",
+        size:"L",
+        price:"$125",
+        p:125,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4123464030014_060_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Seamed Wide-Leg Jumpsuit",
+        color:"Red",
+        type:"Jumpsuit",
+        size:"XL",
+        price:"$180",
+        p:180,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4122011330276_001_b3?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"AG The Deven Wide-Leg Jeans",
+        color:"Black",
+        type:"Jeans",
+        size:"M",
+        price:"$215",
+        p:215,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4110349290089_066_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Let Me Be Puff-Sleeved Blouse",
+        color:"Pink",
+        type:"Tops",
+        size:"M",
+        price:"$98",
+        p:98,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4130652010033_066_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Maeve Bow-Tie Satin Mini Dress",
+        color:"Pink",
+        type:"Dress",
+        size:"XS",
+        price:"$46",
+        p:46,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4113326950078_035_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Pilcro Teddy V-Neck Tunic Sweater",
+        color:"Yellow",
+        type:"Tops",
+        size:"M",
+        price:"$98",
+        p:98,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4130647160096_053_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Maeve Embroidered Lace Mini Dress",
+        color:"Purple",
+        type:"Dress",
+        size:"S",
+        price:"$119",
+        p:119,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4112609690213_050_b4?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Classic Comfy Top",
+        color:"Abstract",
+        type:"Tops",
+        size:"S",
+        price:"$60",
+        p:60,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/65274649_211_b1?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"V. Chapman Margherita Dress",
+        color:"Creme",
+        type:"Dress",
+        size:"S",
+        price:"$435",
+        p:435,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/65004855_053_b4?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Thermal Reformer Jumpsuit",
+        color:"Purple",
+        type:"Jumpsuit",
+        size:"S",
+        price:"$162",
+        p:162,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4113086690043_085_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Pilcro V-Neck Cashmere Sweater",
+        color:"Red",
+        type:"Tops",
+        size:"M",
+        price:"$128",
+        p:128,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4112527770078_001_b4?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Maeve Flouncy Top",
+        color:"Black",
+        type:"Tops",
+        size:"M",
+        price:"$68",
+        p:68,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4114529100142_054_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Pilcro Acid Wash Cardigan",
+        color:"Purple",
+        type:"Jacket/Hoodies",
+        size:"M",
+        price:"$148",
+        p:148,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4130652010027_009_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Ruffled Satin Mini Dress",
+        color:"Abstract",
+        type:"Dress",
+        size:"S",
+        price:"$148",
+        p:148,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4130646420034_050_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Maeve Ruffled Tulle Mini Dress",
+        color:"Purple",
+        type:"Dress",
+        size:"L",
+        price:"$119",
+        p:119,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4114356160009_090_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Floral Embroidered Cardigan",
+        color:"Abstract",
+        type:"Jacket/Hoodies",
+        size:"S",
+        price:"$198",
+        p:198,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4123607630017_049_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Maeve The Margot Kick Flare Pants",
+        color:"Red",
+        type:"Jeans",
+        size:"L",
+        price:"$98",
+        p:98,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4114910730007_000_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Landscape Turtleneck Sweater",
+        color:"Abstract",
+        type:"Tops",
+        size:"XS",
+        price:"$478",
+        p:478,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4110578570050_001_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Embroidered Lace Blouse",
+        color:"Black",
+        type:"Tops",
+        size:"M",
+        price:"$140",
+        p:140,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4110292120209_079_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Gayla Lace Blouse",
+        color:"Yellow",
+        type:"Tops",
+        size:"XL",
+        price:"$160",
+        p:160,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4114529100168_085_b3?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Pilcro Cable-Knit Hoodie",
+        color:"Red",
+        type:"Jacket/Hoodies",
+        size:"M",
+        price:"$148",
+        p:148,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/64427974_012_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Sherpa Sweatshirt",
+        color:"Creme",
+        type:"Tops",
+        size:"M",
+        price:"$192",
+        p:192,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4112609690215_066_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Relaxed Mock Neck Waffle Top",
+        color:"Pink",
+        type:"Tops",
+        size:"M",
+        price:"$80",
+        p:80,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/65626871_045_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Maeve The Margot Kick Flare Pants",
+        color:"Abstract",
+        type:"Jeans",
+        size:"XS",
+        price:"$105",
+        p:105,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4111686200002_001_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Seamed Hoodie Set",
+        color:"Black",
+        type:"Jacket/Hoodies",
+        size:"M",
+        price:"$150",
+        p:150,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4130370060062_001_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Slim Lace Midi Dress",
+        color:"Black",
+        type:"Dress",
+        size:"M",
+        price:"$270",
+        p:270,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4112916210019_012_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Embroidered Floral Buttondown",
+        color:"Creme",
+        type:"Tops",
+        size:"M",
+        price:"$128",
+        p:128,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4113086690055_053_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Maeve Colorblocked Cashmere Sweater",
+        color:"Purple",
+        type:"Tops",
+        size:"L",
+        price:"$170",
+        p:170,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4130264840008_015_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Floral Mini Dress",
+        color:"Abstract",
+        type:"Dress",
+        size:"XL",
+        price:"$165",
+        p:165,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4130647160105_041_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Jacquard Mini Dress",
+        color:"Purple",
+        type:"Dress",
+        size:"L",
+        price:"$385",
+        p:385,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4111347800001_046_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Contrast Eyelet Pullover",
+        color:"Abstract",
+        type:"Tops",
+        size:"XL",
+        price:"$120",
+        p:120,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/66359787_266_b4?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Anthropologie Cropped Wrap Top",
+        color:"Pink",
+        type:"Tops",
+        size:"M",
+        price:"$78",
+        p:78,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4113326950073_702_b3?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Sweetheart Sweater",
+        color:"Yellow",
+        type:"Tops",
+        size:"XS",
+        price:"$59",
+        p:59,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4130657990040_066_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Blazer Mini Dress",
+        color:"Pink",
+        type:"Dress",
+        size:"XS",
+        price:"$148",
+        p:148,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4114529100177_011_c?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Pilcro Utility Sweater Jacket",
+        color:"Creme",
+        type:"Jacket/Hoodies",
+        size:"M",
+        price:"$138",
+        p:138,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4112522160077_062_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Pilcro Split-Neck Top",
+        color:"Purple",
+        type:"Tops",
+        size:"M",
+        price:"$56",
+        p:56,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/65630121_070_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Gauzy Plaid Mini Dress",
+        color:"Creme",
+        type:"Dress",
+        size:"M",
+        price:"$398",
+        p:98,
+    },
+    {
+        image_url:
+            "https://images.urbndata.com/is/image/Anthropologie/4130231803171_001_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720",
+        name:"Byron Lars Carissima Sheath Dress",
+        color:"Black",
+        type:"Dress",
+        size:"S",
+        price:"$528",
+        p:528,
+    },
+    
+    
+]
+
+
+
+var cart = JSON.parse(localStorage.getItem("basketItem")) || [];
+
+clothingData(prodData);
+
+//Sorting by Price
+
+function PriceSort(){
+    var select= document.querySelector("#priceSort").value;
+    console.log(select);
+
+    if (select == "high"){
+        prodData.sort(function (a, b) {
+            return Number(b.p) - Number(a.p);
+
+        });
+    }
+    if(select == "low"){
+        prodData.sort(function (a, b) {
+            return Number(a.p) - Number(b.p);
+
+        });
+    }
+    // console.log(product);
+    clothingData(prodData);
+}
+
+//Clothing Page Data
+
+function clothingData(prodData){
+    document.querySelector("#ProductDisplay").textContent ="";
+    prodData.map(function (elem){
+    
+        var div1  = document.createElement('div');
+        
+        //Product Display
+        var imagediv  = document.createElement('div');
+        var img = document.createElement('img');
+        img.setAttribute('src',elem.image_url);
+        img.setAttribute('id','Product')
+        imagediv.append(img);
+    
+        //Product Details
+        var namediv  = document.createElement('div');
+        var namehead = document.createElement('p');
+        namehead.textContent = elem.name;
+        namediv.append(namehead);
+        
+        var rate  = document.createElement('p');
+        var mrpdiv  = document.createElement('div');
+        rate.textContent = elem.price;
+        mrpdiv.append(rate);
+        
+        var button = document.createElement('button');
+        button.setAttribute('id','btn');
+        button.textContent = "Quick Pick";
+        button.addEventListener("click", function(){
+            addCart(elem);
+        });
+        
+        
+        div1.append(imagediv,namediv,mrpdiv,button); 
+        document.querySelector('#ProductDisplay').append(div1);
+        
+    })
+}
+
+//Cart
+var cart=[];
+
+function addCart(elem){
+    console.log(elem);
+    cart.push(elem);
+    localStorage.setItem("basketItem", JSON.stringify(cart))
+    alert("Added Item to Basket");
+}
